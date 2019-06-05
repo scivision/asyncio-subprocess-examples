a1dc8404be54e3a0860b37a3ec187a194b9efba0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-06-05 13:37:03 -0400

    init Pool example

---
+++ PoolExceptions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Errors are raised with .map of ProcessPoolExecutor or ThreadPoolExecutor only when retrieving the result,
+as per docs:
+https://docs.python.org/3/library/concurrent.futures.html
+
+"""
+import concurrent.futures
+from typing import Iterator
+import os
+
+
+def main(numbers) -> Iterator[int]:
+    """
+    The same behavior occurs with ThreadPoolExecutor and ProcessPoolExecutor:
+    exception not thrown till iterator is iterated to the exception.
+    """
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        results = executor.map(timestwo, numbers, timeout=5)
+
+    return results
+
+
+def timestwo(number: int) -> int:
+    err = number * 2
+
+    if err == 8:
+        raise RuntimeError('The evil value {} has been reached'.format(err))
+
+    return err
+
+
+if __name__ == '__main__':
+    numbers = range(1, 5)
+
+    num2 = main(numbers)
+
+    for n in num2:
+        if n == 6:
+            print('next comes an exception')
